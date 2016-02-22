be8f747d881604869c6ba4dc2ec63164c3415fbc
Martin Landa <landa.martin@gmail.com>
2016-02-22 18:04:59 +0100

    grass cosmetics

--- scripts/grass-trunk-compile.sh
+++ scripts/grass-trunk-compile.sh
@@ -6,24 +6,20 @@ VERSION=7.1.svn
 MAJOR=71
 
 cd /mnt/home/gislab/src/grass_$MAJOR
-svn up 
-if [ "$1" = "f" ] ; then
+svn up
+if [ -f include/Make/Platform.make ] ; then
     make distclean
 fi
-if [ ! -f include/Make/Platform.make ] ; then
-    ./configure \
-	--prefix=/usr/local \
-	--with-postgres --with-postgres-includes=/usr/include/postgresql \
-	--with-gdal --with-proj \
-	--with-nls \
-	--with-cxx --enable-largefile \
-	--with-freetype --with-freetype-includes=/usr/include/freetype2 \
-	--with-sqlite \
-	--with-cairo --with-python \
-	--with-geos --with-pthread --with-lapack --with-blas
-fi
-
-set -e
+./configure \
+    --prefix=/usr/local \
+    --with-postgres --with-postgres-includes=/usr/include/postgresql \
+    --with-gdal --with-proj \
+    --with-nls \
+    --with-cxx --enable-largefile \
+    --with-freetype --with-freetype-includes=/usr/include/freetype2 \
+    --with-sqlite \
+    --with-cairo --with-python \
+    --with-geos --with-pthread --with-lapack --with-blas
 
 make -j2
 make install
