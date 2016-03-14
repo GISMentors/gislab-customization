9c9ffb238170fcabf605a64795d6c88216ef5392
Martin Landa <landa.martin@gmail.com>
2016-03-14 10:54:57 +0100

    create user: force removing user

--- scripts/create-users-b870.sh
+++ scripts/create-users-b870.sh
@@ -7,6 +7,7 @@ for n in `seq 1 21`; do
 	num="$n"
     fi
     user=pc$num
+    sudo rm -f /mnt/home/${user}/.gislab/session.lock
     sudo gislab-deluser -f $user
     sudo gislab-adduser -g PC-$num -l B870 -m $user@gislab.fsv.cvut.cz -p b870 $user
 done
