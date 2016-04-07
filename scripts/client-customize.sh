11156ddc9112bd033745770c95e00334ea9397ce
Martin Landa <landa.martin@gmail.com>
2016-04-07 10:16:59 +0200

    client: rpy2

--- scripts/client-customize.sh
+++ scripts/client-customize.sh
@@ -11,7 +11,7 @@ apt-get install --no-install-recommends --yes gedit geany sqlitebrowser
 ###############
 ### Python libs
 ###############
-apt-get install --yes python-pip g++ python-dev libgdal-dev python-numpy 
+apt-get install --yes python-pip g++ python-dev libgdal-dev python-numpy python-rpy2
 pip install Fiona
 pip install rasterio
 pip install shapely
