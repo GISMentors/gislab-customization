dd1f05ac07a8929b7a89f4df8b5dcda777289dd6
Martin Landa <landa.martin@gmail.com>
2016-04-11 13:58:31 +0200

    install saga and orfeo

--- scripts/client-customize.sh
+++ scripts/client-customize.sh
@@ -2,9 +2,15 @@
 set -e
 
 ##########################
+### Use UbuntuGIS
+##########################
+apt-add-repository --yes ppa:ubuntugis/ubuntugis-unstable
+apt-add-repository --yes ppa:libreoffice/ppa # dh-python
+apt-get update
+
+##########################
 ### Install gedit & geany
 ##########################
-apt-get update
 apt-get install --no-install-recommends --yes gedit geany sqlitebrowser
 # sed -i 's/leafpad/gedit/g' /etc/xdg/xdg-xubuntu/xfce4/panel/default.xml 
 
@@ -23,9 +29,9 @@ pip install OWSLib
 apt-get install --yes postgis
 
 ##########################
-### SAGA
+### SAGA & OTB
 ##########################
-apt-get install --yes saga
+apt-get install --yes saga otb-bin otb-bin-qt python-otb
 
 ##########
 ### Nvidia
@@ -43,22 +49,22 @@ fi
 if [ ! -d /etc/apt/sources.list.d ] ; then
     mkdir /etc/apt/sources.list.d
 fi
-apt-add-repository --yes ppa:landa-martin/gislab-testing # (QGIS 2.8)
+#apt-add-repository --yes ppa:landa-martin/gislab-testing # (QGIS 2.8)
+#apt-get update
 ### apt-add-repository --yes ppa:landa-martin/gislab-gismentors # (QGIS 2.14 + GDAL 2.0)
-apt-get update
 
 ############
 ### QGIS 2.8
 ############
 ### apt-get install --yes --force-yes libgdal20 gdal-bin python-gdal libgeos-3.5.0
-apt-get install --yes --force-yes qgis python-qgis qgis-plugin-grass
+#apt-get install --yes --force-yes qgis python-qgis qgis-plugin-grass
 
 #######################################################
 ### Fix (https://github.com/imincik/gis-lab/issues/402)
 #######################################################
 apt-get install --yes --force-yes lightdm-gtk-greeter=1.1.5-0ubuntu1
 pip install numpy==1.8
-apt-get install libqca2 python-qt4-sql # QGIS 2.14
+apt-get install --yes --force-yes libqca2 python-qt4-sql pyqt4-dev-tools qt4-designer # QGIS 2.14
 
 apt-get --yes --force-yes autoremove
 
