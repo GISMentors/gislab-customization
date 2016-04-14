dee06f35304722e5519d205206e0f0924576cada
Martin Landa <landa.martin@gmail.com>
2016-04-14 19:26:49 +0200

    remove duplicated files regarding master

--- scripts/after-add-ssh-key.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mkdir /mnt/home/${GISLAB_USER}/.ssh
-cp /mnt/home/gislab/.ssh/id_rsa.pub /mnt/home/${GISLAB_USER}/.ssh/authorized_keys
-chown ${GISLAB_USER}:gislabusers /mnt/home/${GISLAB_USER}/.ssh/ -R
-
-exit 0
--- scripts/client-backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo tar cjf /mnt/backup/client-desktop-root-`date -I`.tar.bz2 \
-    /opt/gislab/system/clients/desktop/root
-sudo cp -a /opt/gislab/system/clients/desktop/image \
-   /mnt/backup/client-desktop-image-`date -I`
-
-exit 0
