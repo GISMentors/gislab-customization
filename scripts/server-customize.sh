16ee0033f47d139e3dfebf84008f2e03cd15246d
Martin Landa <landa.martin@gmail.com>
2016-06-10 14:11:05 +0200

    switch to grass72

--- scripts/server-customize.sh
+++ scripts/server-customize.sh
@@ -12,24 +12,24 @@ apt-get install --yes flex bison libproj-dev libtiff-dev mesa-common-dev libglu1
 if [ ! -d $homedir/src ]; then
     mkdir $homedir/src
 fi
-if [ ! -d $homedir/src/grass_71 ] ; then
+if [ ! -d $homedir/src/grass_72 ] ; then
     
-    svn co http://svn.osgeo.org/grass/grass/trunk $homedir/src/grass_71
+    svn co http://svn.osgeo.org/grass/grass/branches/releasebranch_7_2 $homedir/src/grass_72
 fi
-if [ ! -d $homedir/src/geos_35 ] ; then
-    svn co https://svn.osgeo.org/geos/branches/3.5/ $homedir/src/geos_35
-fi
-if [ ! -d $homedir/src/gdal_20 ] ; then
-    svn co https://svn.osgeo.org/gdal/branches/2.0/gdal/ $homedir/src/gdal_20
-fi
+# if [ ! -d $homedir/src/geos_35 ] ; then
+#     svn co https://svn.osgeo.org/geos/branches/3.5/ $homedir/src/geos_35
+# fi
+# if [ ! -d $homedir/src/gdal_20 ] ; then
+#     svn co https://svn.osgeo.org/gdal/branches/2.0/gdal/ $homedir/src/gdal_20
+# fi
 if [ ! -d $homedir/src/qgis_214 ] ; then
     git clone https://github.com/qgis/QGIS.git $homedir/src/qgis_214
     (cd $homedir/src/qgis_214 && git branch release-2_14 origin/release-2_14 && git checkout release-2_14)
 fi
-if [ ! -d $homedir/src/proj_49 ] ; then
-    git clone https://github.com/OSGeo/proj.4.git $homedir/src/proj_49
-    (cd $homedir/src/proj_49 && git branch 4.9 origin/4.9 && git checkout 4.9)
-fi
+# if [ ! -d $homedir/src/proj_49 ] ; then
+#     git clone https://github.com/OSGeo/proj.4.git $homedir/src/proj_49
+#     (cd $homedir/src/proj_49 && git branch 4.9 origin/4.9 && git checkout 4.9)
+# fi
 chown gislab:gislabusers $homedir/src -R
 
 exit 0
